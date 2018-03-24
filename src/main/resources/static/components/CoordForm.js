8626929842f17a604044ae2c7d19b99e3a273e12
Ksenia <ksenia1224@gmail.com>
2018-03-25 01:13:46 +0300

    Add frontend validation

--- src/main/resources/static/components/CoordForm.js
+++ src/main/resources/static/components/CoordForm.js
@@ -1,38 +1,74 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
+
 class CoordForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {x: "", y: "", r: ""};
+        var y = props.y;
+        var yIsValid = this.validateY(y);
+        var x = props.x;
+        var xIsValid = this.validateX(x);
+        // массив одинаков, поэтому я решила не париться, а просто этой же функцией валидировать
+        // Да здравствует говнокод!
+        var r = props.r;
+        var rIsValid = this.validateX(r);
 
+        this.state = {x: x, y: y, r: r, yIsEmpty: true, yIsValid: yIsValid, rIsValid: rIsValid, xIsValid: xIsValid};
         this.onXChange = this.onXChange.bind(this);
         this.onYChange = this.onYChange.bind(this);
         this.onRChange = this.onRChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // Изменение x, на всякий пожарный для всех надо сделать
+    validateY(y) {
+        return y >= -3 && y <= 3;
+    }
+
+    validateX(x) {
+        var arr = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+        x = parseInt(x);
+        return arr.indexOf(x) != -1;
+    }
+
+    // Изменение x, проверка на пустоту, валидация
     onXChange(e) {
         var val = e.target.value;
-        this.setState({x: val});
+        // var valid = this.validateX(val);
+        var valid = this.validateX(val);
+        this.setState({x: val, xIsValid: valid});
     }
 
     onYChange(e) {
         var val = e.target.value;
-        this.setState({y: val});
+        //alert(this.state.yIsEmpty);
+        if (e.target.value.trim().length > 0) {
+            this.setState({yIsEmpty: false})
+        } else {
+            this.setState({yIsEmpty: true})
+        }
+        var valid = this.validateY(val) && this.state.yIsEmpty;
+        this.setState({y: val, yIsValid: valid});
     }
+
     onRChange(e) {
         var val = e.target.value;
-        this.setState({r: val});
+        var valid = this.validateX(val);
+        this.setState({r: val, rIsValid: valid});
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        alert("Y: " + this.state.y + "X: " + this.state.x + "R: " + this.state.r);
+        if (this.state.xIsValid === true && this.state.yIsValid === true && this.state.rIsValid === true) {
+            const data = new FormData(event.target);
+            alert(data);
+            alert("Y: " + this.state.y + " X: " + this.state.x + " R: " + this.state.r);
+        }
+        // alert("Y: " + this.state.y + " X: " + this.state.x + " R: " + this.state.r);
     }
 
     render() {
+        var yColor = this.state.yIsValid === true ? "#9DF69C" : "#F6A19C";
         return (
             <div className="row form-container">
                 <div className="col-sm-8 col-sm-offset-2">
@@ -41,12 +77,14 @@ class CoordForm extends React.Component {
                         <div className="form-group" id="Coord">
                             <div className=" form-check ">
                                 <p className="float-left"><b>X coordinate:</b></p>
-                                <input type="checkbox" name="x" value="-4" id="x1" onChange={this.onXChange}/><label className="form-check-label"
-                                                                                           htmlFor="x1">
+                                <input type="checkbox" name="x" value="-4" id="x1" onChange={this.onXChange}/><label
+                                className="form-check-label"
+                                htmlFor="x1">
                                 -4
                             </label>
-                                <input type="checkbox" name="x" value="-3" id="x2" onChange={this.onXChange}/><label className="form-check-label"
-                                                                                           htmlFor="x2">
+                                <input type="checkbox" name="x" value="-3" id="x2" onChange={this.onXChange}/><label
+                                className="form-check-label"
+                                htmlFor="x2">
                                 -3
                             </label>
                                 <input type="checkbox" name="x" value="-2" id="x3" onChange={this.onXChange}/><label
@@ -80,21 +118,23 @@ class CoordForm extends React.Component {
                             </div>
                             <br/>
 
-                                <div className="">
-                                    <p ><b>Y coordinate:</b></p>
-                                    <input type="text" value={this.state.y} className="form-control" id="inputY"
-                                           placeholder="y" onChange={this.onYChange}/>
-                                </div>
+                            <div className="">
+                                <p><b>Y coordinate:</b></p>
+                                <input type="text" value={this.state.y} className="form-control" id="inputY"
+                                       placeholder="y" onChange={this.onYChange} style={{borderColor: yColor}}/>
+                            </div>
 
                             <br/>
                             <div className=" form-check ">
                                 <p><b>Radius:</b></p>
-                                <input type="checkbox" name="r" value="-4" id="r1" onChange={this.onRChange}/><label className="form-check-label"
-                                                                                           htmlFor="r1">
+                                <input type="checkbox" name="r" value="-4" id="r1" onChange={this.onRChange}/><label
+                                className="form-check-label"
+                                htmlFor="r1">
                                 -4
                             </label>
-                                <input type="checkbox" name="r" value="-3" id="r2" onChange={this.onRChange}/><label className="form-check-label"
-                                                                                           htmlFor="r2">
+                                <input type="checkbox" name="r" value="-3" id="r2" onChange={this.onRChange}/><label
+                                className="form-check-label"
+                                htmlFor="r2">
                                 -3
                             </label>
                                 <input type="checkbox" name="r" value="-2" id="r3" onChange={this.onRChange}/><label
@@ -127,7 +167,8 @@ class CoordForm extends React.Component {
                             </label>
 
                                 <br/>
-                                <p><input id="submit" type="submit" className="submit pull-right" value="Submit"/>
+                                <p><input id="submit" type="submit"
+                                          className="submit pull-right" value="Submit"/>
                                 </p>
                             </div>
                         </div>
@@ -139,9 +180,8 @@ class CoordForm extends React.Component {
 }
 
 
-
 ReactDOM.render(
-    <CoordForm />,
+    <CoordForm/>,
     document.getElementById("app")
 );
 
