de4b6202b2233dca08a882071b7a757262e66763
Tanya <fomina.tatianaaa@yandex.ru>
2018-03-24 21:47:28 +0300

    few more changes

--- src/main/resources/static/components/CoordForm.js
+++ src/main/resources/static/components/CoordForm.js
@@ -6,14 +6,25 @@ class CoordForm extends React.Component {
         super(props);
         this.state = {x: "", y: "", r: ""};
 
-        this.onChange = this.onChange.bind(this);
+        this.onXChange = this.onXChange.bind(this);
+        this.onYChange = this.onYChange.bind(this);
+        this.onRChange = this.onRChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // Изменение x, на всякий пожарный для всех надо сделать
-    onChange(e) {
+    onXChange(e) {
         var val = e.target.value;
         this.setState({x: val});
+    }
+
+    onYChange(e) {
+        var val = e.target.value;
+        this.setState({y: val});
+    }
+    onRChange(e) {
+        var val = e.target.value;
+        this.setState({r: val});
     }
 
     handleSubmit(e) {
@@ -24,45 +35,45 @@ class CoordForm extends React.Component {
     render() {
         return (
             <div className="row form-container">
-                <div className="col-sm-6 col-sm-offset-1">
+                <div className="col-sm-8 col-sm-offset-2">
                     <form method="post" action="" onSubmit={this.handleSubmit}>
                         <h3>Enter point coordinates</h3>
                         <div className="form-group" id="Coord">
                             <div className=" form-check ">
                                 <p className="float-left"><b>X coordinate:</b></p>
-                                <input type="checkbox" name="x" value="-4" id="x1"/><label className="form-check-label"
+                                <input type="checkbox" name="x" value="-4" id="x1" onChange={this.onXChange}/><label className="form-check-label"
                                                                                            htmlFor="x1">
                                 -4
                             </label>
-                                <input type="checkbox" name="x" value="-3" id="x2"/><label className="form-check-label"
+                                <input type="checkbox" name="x" value="-3" id="x2" onChange={this.onXChange}/><label className="form-check-label"
                                                                                            htmlFor="x2">
                                 -3
                             </label>
-                                <input type="checkbox" name="x" value="-2" id="x3"/><label
+                                <input type="checkbox" name="x" value="-2" id="x3" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x3">
                                 -2
                             </label>
-                                <input type="checkbox" name="x" value="-1" id="x4"/><label
+                                <input type="checkbox" name="x" value="-1" id="x4" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x4">
                                 -1
                             </label>
-                                <input type="checkbox" name="x" value="0" id="x5"/><label
+                                <input type="checkbox" name="x" value="0" id="x5" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x5">
                                 0
                             </label>
-                                <input type="checkbox" name="x" value="1" id="x6"/><label
+                                <input type="checkbox" name="x" value="1" id="x6" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x6">
                                 1
                             </label>
-                                <input type="checkbox" name="x" value="2" id="x7"/><label
+                                <input type="checkbox" name="x" value="2" id="x7" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x7">
                                 2
                             </label>
-                                <input type="checkbox" name="x" value="3" id="x8"/><label
+                                <input type="checkbox" name="x" value="3" id="x8" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x8">
                                 3
                             </label>
-                                <input type="checkbox" name="x" value="4" id="x9"/><label
+                                <input type="checkbox" name="x" value="4" id="x9" onChange={this.onXChange}/><label
                                 className="form-check-label" htmlFor="x9">
                                 4
                             </label>
@@ -72,45 +83,45 @@ class CoordForm extends React.Component {
                                 <div className="">
                                     <p ><b>Y coordinate:</b></p>
                                     <input type="text" value={this.state.y} className="form-control" id="inputY"
-                                           placeholder="y"/>
+                                           placeholder="y" onChange={this.onYChange}/>
                                 </div>
 
                             <br/>
                             <div className=" form-check ">
                                 <p><b>Radius:</b></p>
-                                <input type="checkbox" name="r" value="-4" id="r1"/><label className="form-check-label"
+                                <input type="checkbox" name="r" value="-4" id="r1" onChange={this.onRChange}/><label className="form-check-label"
                                                                                            htmlFor="r1">
                                 -4
                             </label>
-                                <input type="checkbox" name="r" value="-3" id="r2"/><label className="form-check-label"
+                                <input type="checkbox" name="r" value="-3" id="r2" onChange={this.onRChange}/><label className="form-check-label"
                                                                                            htmlFor="r2">
                                 -3
                             </label>
-                                <input type="checkbox" name="r" value="-2" id="r3"/><label
+                                <input type="checkbox" name="r" value="-2" id="r3" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r3">
                                 -2
                             </label>
-                                <input type="checkbox" name="r" value="-1" id="r4"/><label
+                                <input type="checkbox" name="r" value="-1" id="r4" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r4">
                                 -1
                             </label>
-                                <input type="checkbox" name="r" value="0" id="r5"/><label
+                                <input type="checkbox" name="r" value="0" id="r5" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r5">
                                 0
                             </label>
-                                <input type="checkbox" name="r" value="1" id="r6"/><label
+                                <input type="checkbox" name="r" value="1" id="r6" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r6">
                                 1
                             </label>
-                                <input type="checkbox" name="r" value="2" id="r7"/><label
+                                <input type="checkbox" name="r" value="2" id="r7" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r7">
                                 2
                             </label>
-                                <input type="checkbox" name="r" value="3" id="r8"/><label
+                                <input type="checkbox" name="r" value="3" id="r8" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r8">
                                 3
                             </label>
-                                <input type="checkbox" name="r" value="4" id="r9"/><label
+                                <input type="checkbox" name="r" value="4" id="r9" onChange={this.onRChange}/><label
                                 className="form-check-label" htmlFor="r9">
                                 4
                             </label>
